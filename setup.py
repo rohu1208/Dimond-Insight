071db81f055872624e18f016998aceb994698a85
rohu1208 <rohan0070k@gmail.com>
2024-10-09 11:34:19 +0530

    first commit

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import find_packages,setup
+from typing import List 
+
+HYPEN_E_DOT='-e .' #trigger setup.py
+
+
+def get_requirements(file_path:str)->List[str]:
+    requirements=[]
+    with open(file_path) as file_obj:
+        requirements=file_obj.readlines()
+        requirements=[req.replace("\n","") for req in requirements]
+
+        if HYPEN_E_DOT in requirements:
+            requirements.remove(HYPEN_E_DOT)
+        return requirements
+setup(
+    name='DiamondPricePrediction',
+    version='0.0.1',
+    author='rohan kaitake',
+    authon_email='rohankaitake5@gmail.com',
+    install_requires=get_requirements("requirements.txt"),
+    packages=find_packages()
+)
